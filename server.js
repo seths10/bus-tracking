d3645259ef5be91fa6a0aee6c5507381e45fd327
Seth Addo <59029978+seths10@users.noreply.github.com>
2023-04-30 06:16:29 +0000

    removed database integration

--- server.js
+++ server.js
@@ -1,35 +1,36 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'bus_nodes',
-  password: 'admin',
-  port: 5432,
-});
-
 const app = express();
-app.use(bodyParser.json());
 
+let data = [];
 
 app.post('/api/uplink', (req, res) => {
   const { payload_fields } = req.body.uplink_message.decoded_payload;
+  const deviceID = req.body.device_ids.device_id;
 
-  pool.query(
-    'INSERT INTO payload_data (altitude, latitude, longitude, sat) VALUES ($1, $2, $3, $4)',
-    [payload_fields.altitude, payload_fields.latitude, payload_fields.longitude, payload_fields.sat],
-    (err, result) => {
-      if (err) {
-        console.error('Error executing query', err.stack);
-        res.status(500).send('Error executing query');
-      } else {
-        console.log('Payload data saved successfully');
-        res.status(200).send('Payload data saved successfully');
-      }
-    }
-  );
+  console.log(`Received data from device ${deviceID}: ${payload_fields}`);
+
+  const altitude = payload_fields.altitude;
+  const latitude = payload_fields.latitude;
+  const longitude = payload_fields.longitude;
+  const sat = payload_fields.sat;
+
+  const newData = {
+    deviceID,
+    altitude,
+    latitude,
+    longitude,
+    sat,
+    timestamp: Date.now()
+  };
+
+  data.push(newData);
+
+  res.sendStatus(200);
+
+});
+
+app.get('/api/data', (req, res) => {
+  res.json(data);
 });
 
 const port = process.env.PORT || 3000;
